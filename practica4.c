db67065944d929ee33cad9e168fc09e16d866aee
Ricardo Maximiliano Victoria <ricardo.max@ciencias.unam.mx>
2024-03-18 18:13:07 -0600

    Ya esta la resta, y modique la suma para que devolviera solo 32 bits en la resta porque asi dice en la practica y nos ahorramos mucho problema osiosi

--- practica4.c
+++ practica4.c
@@ -3,40 +3,60 @@
 #include <string.h>
 #include <math.h>
 
-void adicion(char opA[], char opB[], int lA, int lB){
-  int acarreo = 0, auxiliar;
-  char resultado[32];
-  
-  while  (lB > 0){    
-    auxiliar = (opA[lA-1] - '0') + (opB[lB-1] - '0') + acarreo;
-    if (auxiliar >= 2){
-      acarreo = 1;
-      auxiliar = auxiliar-2;
-    }
-    else{
-      acarreo = 0;
-    }
-    resultado[lA] = auxiliar + '0';
-    lA--;
-    lB--;  
-  }
 
-  if (acarreo == 1){
-    resultado[0] = '1';
-  }
-  else{
-    for (int i = 0; i < strlen(resultado)-1; i++){
-      resultado[i] = resultado[i+1];
-    }
-    resultado[strlen(resultado) - 1] = '\0';
-  }
+
+void adicion(char opA[], char opB[], int lA, int lB) {
+    int acarreo = 0, auxiliar;
+    char *resultado = (char *)malloc(32 * sizeof(char)); // Reserva memoria dinámica para el resultado
   
-  printf("La suma de los números es %s\n", resultado);
+    while  (lB > 0) {    
+        lB = lB-1;
+        lA = lA-1;
+        printf("Al iniciar el ciclo el valor de lA es %i y el valor de lB es %i\n", lA, lB);
+        auxiliar = (opA[lA] - '0') + (opB[lB] - '0') + acarreo;
+        if (auxiliar >= 2){
+            acarreo = 1;
+            auxiliar = auxiliar-2;
+        }
+        else{
+            acarreo = 0;
+        }
+        resultado[lA] = auxiliar + '0';
+        printf("El indice %i del resultado es %c\n", lA, resultado[lA]);
+        printf("Al terminar el ciclo el valor de lA es %i y el valor de lB es %i\n\n", lA, lB);
+    }
+  
+    printf("El resultado es %s\n", resultado);
+    free(resultado); // Libera la memoria asignada dinámicamente
 }
+
+
+
 
 void resta(char opA[], char opB[]){
-  return;
+
+  int yaHayUno=0;
+
+  for (int i = 31; i >= 0; i--){
+    if (yaHayUno == 1){
+      if (opB[i] == '0' ){
+        opB[i] = '1';
+      } else if (opB[i] == '1'){
+        opB[i] = '0';
+      }
+    } else if(yaHayUno == 0){
+      if (opB[i] == '1'){
+        yaHayUno = 1;
+      }
+    }    
+  }
+
+  printf("El complemento A2 de opB es %s\n", opB);
+
+  adicion(opA, opB, 32, 32);
 }
+
+
 
 void bitwiseAnd(char opA[], char opB[]){
   printf("\nAND de los números es ");
@@ -90,14 +110,17 @@ void menorQue(char opA[], char opB[]){
 }
 
 int main(int argc, char *argv[]){
+
+  if (argc != 4){
+    printf("Número no válido de argumentos. Ingresar la clave de la operación a realizar seguida de los operandos (32 bits)");
+    return 0;
+  }
+
   int lengthA = strlen(argv[2]);
   int lengthB = strlen(argv[3]);
   int lengthOp = strlen(argv[1]);
   
-  if (argc != 4){
-    printf("\nNúmero no válido de argumentos. Ingresar la clave de la operación a realizar seguida de los operandos (32 bits)");
-    return 0;
-  }
+  
   if (lengthA != 32 || lengthB != 32 || lengthOp != 3){
     printf("\nLos operandos deben ser cadenas de 32 bits y la operación a realizar debe tener 3 bits.");
     return 0;
@@ -117,7 +140,7 @@ int main(int argc, char *argv[]){
   }
   //Pendiente
   if (strcmp("011",argv[1]) == 0){
-    resta(argv[2],argv[3],lengthA,lengthB);
+    resta(argv[2],argv[3]);
   }
   //Función de igualdad completa
   if (strcmp("100",argv[1]) == 0){
