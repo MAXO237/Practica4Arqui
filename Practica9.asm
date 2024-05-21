a5254bd8f4709a94c4509a9aa3ca42e5ac56318d
Ricardo Maximiliano Victoria <ricardo.max@ciencias.unam.mx>
2024-05-21 17:21:01 -0600

    La ventana ahora ya pregunta si quieres salir

--- Practica9.asm
+++ Practica9.asm
@@ -1,9 +1,10 @@
 .data
 buffer: .space 80   #Aparta 80 bytes de memoria como buffer para el input
-prompt: .asciiz "Este es el prompt, escribe un comando:> "
+prompt: .asciiz "\nEste es el prompt, escribe un comando:> "
 error_comando_no_encontrado: .asciiz "Error, el comando introducido no existe.\nPor favor intenta de nuevo."
 exit: .asciiz "exit"
 chiste1: .asciiz "Persona 1 - ¿Qué le dijo una papa a otra papa?\nPersona 2 - No sé, ¿qué le dijo?\nPersona 1 - Hola ( :,D )"
+pregunta_si_quiere_salir: .asciiz "¿Seguro que quieres salir?"
 
 .text
 .globl main
@@ -16,6 +17,7 @@ main:
 	syscall			#Se imprime el prompt
 	
 fin:
+	la $a0, pregunta_si_quiere_salir
 	li $v0, 50
 	syscall
 	beq $a0 1 main
